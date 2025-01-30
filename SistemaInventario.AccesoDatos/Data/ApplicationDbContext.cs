26e2fd6857abe56a3cc4c3fac6f2eedf412ee43d
andresfelipe79 <andrupapas@hotmail.com>
2025-01-30 16:30:21 -0500

    configuracion proyecto

--- SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
+++ SistemaInventario.AccesoDatos/Data/ApplicationDbContext.cs
@@ -7,6 +7,7 @@ namespace SistemaInventario.AccesoDatos.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
